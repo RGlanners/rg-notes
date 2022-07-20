4d60e84e99c9f9eed3f6adeba59fa2267a874813
Robert Glanville <rob@vaultplatform.com>
2022-07-21 00:16:19 +0100

    Add MUI theme provider and Apollo Provider.

--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,26 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
+
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "@mui/system";
+import { theme } from "./theme";
+
+const client = new ApolloClient({
+  uri: "https://content-redfish-61.hasura.app/v1/graphql",
+  cache: new InMemoryCache(),
+});
 
 const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
+  document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <ThemeProvider theme={theme}>
+        <App />
+      </ThemeProvider>
+    </ApolloProvider>
   </React.StrictMode>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/theme.ts
@@ -0,0 +1,13 @@
+import { createTheme } from "@mui/material";
+
+export const theme = createTheme({
+    typography: {
+        fontFamily: "'Roboto', 'Oxygen', 'Ubuntu', 'Cantarell', 'Fira Sans', 'Droid Sans', 'Helvetica Neue', sans-serif;",
+        h1: {}
+    },
+    palette: {
+        primary: { main: '#4287f5' },
+        secondary: { main: '#42b0f5' },
+
+    }
+});
